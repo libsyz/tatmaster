42bafa26025e5b1b8be2a4bdb9fd041ad5c8a41f
Miguel Jimenez <denkatalabeno@gmail.com>
2017-06-13 12:56:36 +0200

    faker master implemented

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Create Artists Seed
+
+15.times do
+
+  # n = Faker::Friends.character
+  # i = Faker::Hipster.sentence(3, false, 4)
+  r = [100, 150, 300, 250].sample
+  l = ["Barcelona", "Paris", "London", "Berlin", "Madrid", "Rome", "Geneva"].sample
+
+  p Artist.new(rate: r, location: l)
+
+
+    #   t.string   "name"
+    # t.text     "info"
+    # t.integer  "rate"
+    # t.integer  "location"
+
+end
+
