e24820471ab8bb1776beb9348516b20916a29aac
Lauren DeFrancesco <lauren209@live.com>
2017-06-13 12:56:29 +0200

    seeding 45 fake tattoos

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# tattoos
+
+
+
+45.times do
+  tat = { name: Faker::Pokemon.name,
+           description: Faker::Hipster.sentences(1),
+           style: Faker::Hipster.sentence(3)
+  }
+  p Tattoo.new(tat)
+
+end
