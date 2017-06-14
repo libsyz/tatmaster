577a69cee423f972540b741d06c93e69637f817c
Miguel Jimenez <denkatalabeno@gmail.com>
2017-06-14 13:07:31 +0200

    Facebook authentication implemented

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,11 +9,11 @@ class User < ApplicationRecord
   def self.find_for_facebook_oauth(auth)
     user_params = auth.slice(:provider, :uid)
     user_params.merge! auth.info.slice(:email, :first_name, :last_name)
+    user_params[:email] = auth.fetch(:email,"#{user_params[:uid]}@#{user_params[:provider]}.com")
     user_params[:facebook_picture_url] = auth.info.image
     user_params[:token] = auth.credentials.token
     user_params[:token_expiry] = Time.at(auth.credentials.expires_at)
     user_params = user_params.to_h
-
     user = User.find_by(provider: auth.provider, uid: auth.uid)
     user ||= User.find_by(email: auth.info.email) # User did a regular sign up in the past.
     if user
