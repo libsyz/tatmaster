3182934b313aef00764862f701b55f3b6f4dbe32
Courtney Treweek <c.k.treweek@gmail.com>
2017-06-14 12:58:00 +0200

    implement geocoder

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -3,18 +3,26 @@ class ArtistsController < ApplicationController
 
   def index
     @artists = Artist.all
-    @studio = Studio.where.not(latitude: nil, longitude: nil)
-    @hash = Gmaps4rails.build_markers(@flats) do |flat, marker|
-      marker.lat flat.latitude
-      marker.lng flat.longitude
+    # @studio = Studio.where.not(latitude: nil, longitude: nil)
+    @artist2 = Artist.where.not(latitude: nil, longitude: nil)
+    @artist2.each do |artist|
+      if artist.location == params[:location]
+        @hash = Gmaps4rails.build_markers(artist) do |flat, marker|
+          marker.lat flat.latitude
+          marker.lng flat.longitude
+        end
+      end
     end
   end
-end
 
-def show
-  @artist = Artist.find(params[:id])
+
+  def show
+    @artist = Artist.find(params[:id])
+    @artist2 = Artist.find(params[:location])
     # @appointment = Appointment.new
-    @studio = Studio.find(params[:studio_id])
-    @studio_coordinates = { lat: @studio.latitude, lng: @studio.longitude}
+    # @studio = Studio.find(params[:studio_id])
+    # @studio_coordinates = { lat: @studio.latitude, lng: @studio.longitude}
+    @artist2_coordinates = { lat: @artist2.latitude, lng: @artist2.longitude}
+    raise
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170614091938) do
+ActiveRecord::Schema.define(version: 20170614102339) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(version: 20170614091938) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "location"
+    t.float    "latitude"
+    t.float    "longitude"
     t.index ["studio_id"], name: "index_artists_on_studio_id", using: :btree
   end
 
